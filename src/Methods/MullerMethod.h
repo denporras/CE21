43b72ad981c66445c5dad60eb5c9c89fbb5449c5
kevin <kevinalfa17@gmail.com>
2017-09-10 18:27:24 -0600

    added muller method

---
+++ src/Methods/MullerMethod.h
@@ -0,0 +1,15 @@
+/*
+ * MullerMethod.h
+ *
+ *  Created on: 10 de set. de 2017
+ *      Author: kevin
+ */
+
+#ifndef METHODS_MULLERMETHOD_H_
+#define METHODS_MULLERMETHOD_H_
+
+
+
+
+
+#endif /* METHODS_MULLERMETHOD_H_ */
